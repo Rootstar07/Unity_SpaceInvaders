45842c1d233ac8e0ed22ab10747f9ef37f122a9c
GeunTae KIM <60951051+Rootstar07@users.noreply.github.com>
2020-12-15 19:14:00 +0900

    Bullet
    쏠수있서

---
+++ SpaceIndavers/Assets/Scripts/Bullet.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    public float speed = 30;
+
+    private Rigidbody2D rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        rigidbody = GetComponent<Rigidbody2D>();
+
+        rigidbody.velocity = Vector2.up * speed;
+
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "Wall")
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+
+}
---
+++ SpaceIndavers/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+
+    public static SoundManager Instance = null;
+
+    public AudioClip alienBuzz1;
+    public AudioClip alienBuzz2;
+    public AudioClip alienDies;
+    public AudioClip bulletfire;
+    public AudioClip shipexplosion;
+
+    private AudioSource soundEffectAudio;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else if (Instance != null)
+        {
+            Destroy(gameObject);
+        }
+
+        AudioSource thesource = GetComponent<AudioSource>();
+        soundEffectAudio = thesource;
+    }
+
+    public void PlayOneShot(AudioClip clip)
+    {
+        soundEffectAudio.PlayOneShot(clip);
+    }
+
+
+}
